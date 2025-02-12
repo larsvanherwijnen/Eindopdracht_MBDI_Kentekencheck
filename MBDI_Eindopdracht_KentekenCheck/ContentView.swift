955aeba85528777aa2465ad61223c156ac4cb678
Lars van Herwijnen <lars.druten@gmail.com>
2025-02-12 12:10:08 +0100

    Initial Commit

---
+++ MBDI_Eindopdracht_KentekenCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MBDI_Eindopdracht_KentekenCheck
+//
+//  Created by Lars van Herwijnen on 12/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MBDI_Eindopdracht_KentekenCheck/MBDI_Eindopdracht_KentekenCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_Eindopdracht_KentekenCheckApp.swift
+//  MBDI_Eindopdracht_KentekenCheck
+//
+//  Created by Lars van Herwijnen on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_Eindopdracht_KentekenCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
